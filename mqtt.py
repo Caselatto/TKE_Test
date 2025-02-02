3d839a665041afbe708f5cd5963261e68a44eb64
Caselatto <50884318+Caselatto@users.noreply.github.com>
2025-02-02 16:43:59 -0300

    defines

--- mqtt.py
+++ mqtt.py
@@ -1,12 +1,17 @@
 import paho.mqtt.client as mqtt
 import ssl
 
+MQTT_USR = "master"
+MQTT_PSW = "Abc123**"
+MQTT_TOPIC = "elevator_1"
+MQTT_BROKER = "3d04701073de45d6a59a96eaa0b0d39e.s1.eu.hivemq.cloud"
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code):
     print(f"Connected with result code {reason_code}")
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("Elevator_1")
+    client.subscribe(MQTT_TOPIC)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -15,7 +20,7 @@ def on_message(client, userdata, msg):
 mqttc = mqtt.Client()
 
 # Definir o usuário e a senha
-mqttc.username_pw_set("master", "Abc123**")
+mqttc.username_pw_set(MQTT_USR, MQTT_PSW)
 
 # Definir callbacks
 mqttc.on_connect = on_connect
@@ -25,7 +30,7 @@ mqttc.on_message = on_message
 mqttc.tls_set(certfile=None, keyfile=None, tls_version=ssl.PROTOCOL_TLSv1_2)
 
 # Conectar ao broker usando TLS
-mqttc.connect("3d04701073de45d6a59a96eaa0b0d39e.s1.eu.hivemq.cloud", 8883, 60)
+mqttc.connect(MQTT_BROKER, 8883, 60)
 
 # Bloquear chamada que processa tráfego de rede, despacha callbacks e lida com reconexões.
 mqttc.loop_forever()
